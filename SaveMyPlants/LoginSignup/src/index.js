6b7f2bd5ec24e7c0c99995260a796b47802e1b12
Teresa Moser <moserte@byui.edu>
2024-05-29 23:26:26 -0600

    save plant-list fixes

--- SaveMyPlants/LoginSignup/src/index.js
+++ SaveMyPlants/LoginSignup/src/index.js
@@ -1,25 +1,25 @@
 const express=require("express")
 const app=express()
-// const path=require("path")
+const path=require("path")
 const collection=require("./mongodb")
 
-// const htmlPath=path.join(__dirname, './html')
+const htmlPath=path.join(__dirname, '/main-index.html')
 
 app.use(express.json())
-app.set("view engine")
-app.set("views")
+app.set("view engine","html")
+app.set("views", htmlPath)
 app.use(express.urlencoded({extended:false}))
 
 
-app.get("login.html",(res)=>{
+app.get("/login.html",(res,req)=>{
     res.render("login")
 })
 
-app.get("signup.html",(res)=>{
+app.get("/signup.html",(res,req)=>{
     res.render("signup")
 })
 
-app.post("signup.html", async(req, res)=>{
+app.post("/signup.html", async(req, res)=>{
 
     const data={
         name:req.body.name,
@@ -27,14 +27,14 @@ app.post("signup.html", async(req, res)=>{
         } 
 
     await collection.insertMany([data])
-    res.render("main-index.html")    
+    res.render("/main-index.html")    
 })
 
-app.post("login.html", async(req, res)=>{
+app.post("/login.html", async(req, res)=>{
     try{
        const check=await collection.findOne({name:req.body.name})
             if(check.password===req.body.password){
-                res.render("main-index.html")
+                res.render("/main-index.html")
             }
             else{
                 res.send("wrong password")
--- SaveMyPlants/scripts/plant-list.js
+++ SaveMyPlants/scripts/plant-list.js
@@ -1,61 +1,61 @@
-const url = "data/members.json"
+const url = "data/PlantData.json"; 
 const cards = document.querySelector('#cards');
 
-async function getDirectoryData() {
+async function getListData() {
   const response = await fetch(url);
   const data = await response.json();
-  console.table(data.members);
-  displayDirectory(data.members);
-  }
+  console.table[data.PlantData];
+  displayList[data.PlantData];
+  };
 
-getDirectoryData();
+getListData();
 
-const displayDirectory = (members) => {
+const displayList = (PlantData) => {
  
-  members.forEach((members) => {
+      PlantData.forEach((PlantData) => {
 
-    let card = document.createElement('section');
-    let icon = document.createElement('img');
-    let name = document.createElement('h2'); 
-    let address = document.createElement('p');
-    let phone = document.createElement('p');
-    let web = document.createElement('p');
-    let level = document.createElement('p');
+          let card = document.createElement('section');
+          let imageUrl = document.createElement('img');
+          let name = document.createElement('h2'); 
+          let type= document.createElement('p');
+          let water = document.createElement('p');
+          let light = document.createElement('p');
+          let group = document.createElement('p');
 
-    name.textContent = `${members.name}`;
-    address.innerHTML = `<strong>Address</strong>: ${members.address}`;
-    phone.innerHTML = `<strong>Phone #</strong>: ${members.phone}`;
-    web.innerHTML = `<strong>Website </strong>: ${members.website}`;
-    level.innerHTML = `<strong>Membership Level </strong>: ${members.membershiplevel}`;
+          name.textContent = `${PlantData.name}`;
+          type.innerHTML = `<strong>Type: </strong>: ${PlantData.type}`;
+          water.innerHTML = `<strong>Water: </strong>: ${PlantData.water}`;
+          light.innerHTML = `<strong>Light: </strong>: ${PlantData.light}`;
+          group.innerHTML = `<strong>Group: </strong>: ${PlantData.group}`;
 
-    icon.setAttribute('src', members.image);
-    icon.setAttribute('alt', `logo ${members.name}`); 
-    icon.setAttribute('loading', 'lazy');
-    icon.setAttribute('width', '200');
-    icon.setAttribute('height', '100');
+          imageUrl.setAttribute('src', PlantData.image);
+          imageUrl.setAttribute('alt', `logo ${PlantData.name}`); 
+          imageUrl.setAttribute('loading', 'lazy');
+          imageUrl.setAttribute('width', '200');
+          imageUrl.setAttribute('height', '100');
 
-    card.appendChild(icon);
-    card.appendChild(name); 
-    card.appendChild(address);
-    card.appendChild(phone);
-    card.appendChild(web);
-    card.appendChild(level);
+          card.appendChild(imageUrl);
+          card.appendChild(name); 
+          card.appendChild(type);
+          card.appendChild(water);
+          card.appendChild(light);
+          card.appendChild(group);
 
-    cards.appendChild(card);
-  });
+          cards.appendChild(card);
+        });
 
     const gridbutton = document.querySelector("#grid");
     const listbutton = document.querySelector("#list");
     const display = document.querySelector("article");
 
-    gridbutton.addEventListener("click", () => {
-      display.classList.add("grid");
-      display.classList.remove("list");
-    });
+        gridbutton.addEventListener("click", () => {
+          display.classList.add("grid");
+          display.classList.remove("list");
+        });
 
-    listbutton.addEventListener("click", showList); 
-    function showList() {
-      display.classList.add("list");
-      display.classList.remove("grid");
-    }
-}
+        listbutton.addEventListener("click", showList); 
+        function showList() {
+          display.classList.add("list");
+          display.classList.remove("grid");
+        };
+  };
